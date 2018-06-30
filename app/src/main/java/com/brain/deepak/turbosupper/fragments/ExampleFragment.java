0b5086e6b846052a9a2536043bad8beb67960a97
Brian Fischer <ratfr0@rat-u16.04>
2018-06-30 18:31:08 -0400

    added fragment transactions to tab activity

---
+++ app/src/main/java/com/brain/deepak/turbosupper/fragments/ExampleFragment.java
@@ -0,0 +1,103 @@
+package com.brain.deepak.turbosupper.fragments;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.brain.deepak.turbosupper.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Activities that contain this fragment must implement the
+ * {@link ExampleFragment.OnFragmentInteractionListener} interface
+ * to handle interaction events.
+ * Use the {@link ExampleFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class ExampleFragment extends Fragment {
+    private static final String ARG_TEXT = "text";
+
+    private String text;
+
+    private OnFragmentInteractionListener mListener;
+
+    public ExampleFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @return A new instance of fragment ExampleFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static ExampleFragment newInstance(String text) {
+        ExampleFragment fragment = new ExampleFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_TEXT, text);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            text = getArguments().getString(ARG_TEXT);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View inflatedView = inflater.inflate(R.layout.fragment_example, container, false);
+        ((TextView) inflatedView.findViewById(R.id.exampleFragment_text)).setText(text);
+        return inflatedView;
+    }
+
+    // TODO: Rename method, update argument and hook method into UI event
+    public void onButtonPressed(Uri uri) {
+        if (mListener != null) {
+            mListener.onFragmentInteraction(uri);
+        }
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        if (context instanceof OnFragmentInteractionListener) {
+            mListener = (OnFragmentInteractionListener) context;
+        } else {
+            Log.w("ExampleFragment", "onAttach: " + context.toString() + " should implement OnFragmentInteractionListener");
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+
+    /**
+     * This interface must be implemented by activities that contain this
+     * fragment to allow an interaction in this fragment to be communicated
+     * to the activity and potentially other fragments contained in that
+     * activity.
+     * <p>
+     * See the Android Training lesson <a href=
+     * "http://developer.android.com/training/basics/fragments/communicating.html"
+     * >Communicating with Other Fragments</a> for more information.
+     */
+    public interface OnFragmentInteractionListener {
+        // TODO: Update argument type and name
+        void onFragmentInteraction(Uri uri);
+    }
+}
